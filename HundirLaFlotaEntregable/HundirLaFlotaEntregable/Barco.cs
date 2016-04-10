708f2dd142a98588ee2d298d5250d6beda720077
Eduardo Simón Picón <Eduardo Simn Picn>
2016-04-10 15:36:38 +0200

    Añadido el proyecto base sobre el que trabajar

---
+++ HundirLaFlotaEntregable/HundirLaFlotaEntregable/Barco.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HundirLaFlotaEntregable
+{
+    class Barco
+    {
+
+        // Atributos indicados en el enunciado que han de estar en esta clase
+
+        // CÓDIGO A COMPLETAR
+
+        public Barco(int f, int c, Orientacion orientacion, int tamanyo, int tocadas)
+        {
+            // CÓDIGO A COMPLETAR
+
+        }
+
+
+        // Getters y setters necesarios para los atributos indicados en la descripción 
+        //   del juego ...
+
+        // CÓDIGO A COMPLETAR
+
+        // Método tocaBarco() que debe calcular lo que 
+        //   se indica en el enunciado.
+
+        // CÓDIGO A COMPLETAR
+
+        // Método toString() que debe devolver un String con el
+        //   contenido que se indica en el enunciado.
+
+            // CÓDIGO A COMPLETAR
+
+    }
+}
