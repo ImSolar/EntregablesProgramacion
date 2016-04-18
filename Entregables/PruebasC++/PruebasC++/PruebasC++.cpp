b69b18c3399c877e7572e6b074fb9c444daf2302
Eduardo Simón Picón <Eduardo Simn Picn>
2016-04-18 12:38:19 +0200

    Hecho el metodo ordenacion por seleccion no recursivo

---
+++ Entregables/PruebasC++/PruebasC++/PruebasC++.cpp
@@ -0,0 +1,21 @@
+// PruebasC++.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	bool hola = true;
+
+
+	cout << "el bool ocupa " << sizeof hola << endl;
+
+	cin;
+
+
+
+    return 0;
+}
+
--- Entregables/ordenacionSeleccion/ordenacionSeleccion/Program.cs
+++ Entregables/ordenacionSeleccion/ordenacionSeleccion/Program.cs
@@ -8,8 +8,43 @@ namespace ordenacionSeleccion
 {
     class Program
     {
+        public static int[] miArray = { 23, 3, 6, 36, 7, 5 };
         static void Main(string[] args)
         {
+            OrdenacionPorSeleccion(miArray);
+            ImprimeArray(miArray);
+            Console.ReadKey();
+
+        }
+
+        public static void ImprimeArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ", ");
+            }
+        }
+
+        public static void OrdenacionPorSeleccion(int[] array)
+        { 
+            // for de fuera que nos da el array
+            for (int i = 0; i < array.Length; i++)
+            {
+                int posicionMinima = i;
+
+                    for (int j = i +1; j < array.Length; j++)
+                        {
+                            if (array[posicionMinima] > array[j])
+                                posicionMinima = j;                 
+                        }
+
+                        if(posicionMinima != i)
+                        {
+                            int aux = array[i];
+                            array[i] = array[posicionMinima];
+                            array[posicionMinima] = aux;
+                        }
+             }
         }
     }
 }
