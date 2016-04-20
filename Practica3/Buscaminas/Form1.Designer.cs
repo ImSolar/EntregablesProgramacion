dbbfc7be5fc0257fb7c032e41838d041e01eb811
Eduardo Simón Picón <Eduardo Simn Picn>
2016-04-20 15:19:06 +0200

    Iniciado el proyecto buscaminas

---
+++ Practica3/Buscaminas/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace BuscaMinas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nfilasNUD = new System.Windows.Forms.NumericUpDown();
+            this.ncolumnasNUD = new System.Windows.Forms.NumericUpDown();
+            this.porcentajeMinasNUD = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nfilasNUD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ncolumnasNUD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.porcentajeMinasNUD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nfilasNUD
+            // 
+            this.nfilasNUD.Location = new System.Drawing.Point(36, 8);
+            this.nfilasNUD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nfilasNUD.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nfilasNUD.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nfilasNUD.Name = "nfilasNUD";
+            this.nfilasNUD.Size = new System.Drawing.Size(34, 20);
+            this.nfilasNUD.TabIndex = 0;
+            this.nfilasNUD.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // ncolumnasNUD
+            // 
+            this.ncolumnasNUD.Location = new System.Drawing.Point(124, 8);
+            this.ncolumnasNUD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ncolumnasNUD.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.ncolumnasNUD.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.ncolumnasNUD.Name = "ncolumnasNUD";
+            this.ncolumnasNUD.Size = new System.Drawing.Size(34, 20);
+            this.ncolumnasNUD.TabIndex = 1;
+            this.ncolumnasNUD.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // porcentajeMinasNUD
+            // 
+            this.porcentajeMinasNUD.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.porcentajeMinasNUD.Location = new System.Drawing.Point(123, 33);
+            this.porcentajeMinasNUD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.porcentajeMinasNUD.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.porcentajeMinasNUD.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.porcentajeMinasNUD.Name = "porcentajeMinasNUD";
+            this.porcentajeMinasNUD.Size = new System.Drawing.Size(34, 20);
+            this.porcentajeMinasNUD.TabIndex = 2;
+            this.porcentajeMinasNUD.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Filas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Columnas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 35);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Porcentaje de Minas";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ya!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 61);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.porcentajeMinasNUD);
+            this.Controls.Add(this.ncolumnasNUD);
+            this.Controls.Add(this.nfilasNUD);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BuscaMinas VJ1208";
+            ((System.ComponentModel.ISupportInitialize)(this.nfilasNUD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ncolumnasNUD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.porcentajeMinasNUD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nfilasNUD;
+        private System.Windows.Forms.NumericUpDown ncolumnasNUD;
+        private System.Windows.Forms.NumericUpDown porcentajeMinasNUD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Practica3/Buscaminas/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BuscaMinas
+{
+    public partial class Form1 : Form
+    {
+        static int N = 0, M = 0;
+        Random rand;
+        public bool[,] visitadas; // constructor
+        public Button[,] campoBotones;
+        int[,] m; // 9: mina;0-8:minas alrededor
+        private int Porcentaje;
+
+        public Form1() {
+            InitializeComponent();
+            rand = new Random();
+        }
+
+        private void ReiniciaJuego() {
+            visitadas = new bool[N, M];  // De las filas y columnas 1 a la N-1
+            campoBotones = new Button[N, M];  // De las filas y columnas 1 a la N-1
+            m = new int[N, M]; // 9: mina; 0-8:minas alrededor
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++) {
+                    visitadas[i, j] = false;
+                    m[i, j] = 0;
+                }
+            for (int i = 0; i < N; i++) visitadas[i, 0] = visitadas[i, M-1] = true;
+            for (int i = 0; i < M; i++) visitadas[0, i] = visitadas[N-1, i] = true;
+            creaCampo();
+            ubicaMinas();
+            cuentaMinas();
+        }
+
+        private void ReiniciaControl() {
+            for (int i = 1; i < N-1; i++)
+                for (int j = 1; j < M-1; j++)
+                    Controls.Remove(campoBotones[i, j]);
+        }
+
+        private void creaCampo() {
+            int y = 75; int x;
+            for (int i = 1; i < N-1; i++) {
+                x = 10;
+                for (int j = 1; j < M-1; j++) {
+                    campoBotones[i, j] = new Button();
+                    campoBotones[i, j].Tag = i * 100 + j;
+                    campoBotones[i, j].Left = x;
+                    campoBotones[i, j].Top = y;
+                    campoBotones[i, j].Height = campoBotones[i, j].Width = 25;
+                    x += 25;
+                    campoBotones[i, j].MouseDown += OnClickButton;
+                    Controls.Add(campoBotones[i, j]);
+                }
+                y += 25;
+            }
+        } // fin metodo
+
+        protected void OnClickButton(object sender, MouseEventArgs e) {
+            Button b = (Button)sender;
+            int fila = Convert.ToInt32(b.Tag) / 100;
+            int columna = Convert.ToInt32(b.Tag) % 100;
+            switch (e.Button) {
+                case MouseButtons.Left:
+                    if (b.Text != "x") {
+                        if (m[fila, columna] == 9)
+                        {
+                            b.Text = "B";
+                            b.BackColor = Color.DarkRed;
+                            verMinas();
+                            b.Refresh();
+                            MessageBox.Show("¡Boooooooooooooomba!");
+                            ReiniciaControl();
+                            ReiniciaJuego();
+                        } else {
+                            if (m[fila, columna] != 0) {
+                                b.Text = "" + m[fila, columna];
+                                visitadas[fila, columna] = true;
+                                b.Enabled = false;
+                            }
+                            else escampa(fila, columna);
+                        }
+                    }
+                    break;
+                case MouseButtons.Right:
+                    if (b.Text == "x") { 
+                        b.Text = ""; 
+                        visitadas[fila, columna] = false; 
+                    } else { 
+                        b.Text = "x"; 
+                        visitadas[fila, columna] = true; 
+                    }
+                    if (esGanador()) {
+                        MessageBox.Show("¡Has derrotado al campo de minas!");
+                        ReiniciaControl();
+                        ReiniciaJuego();
+                    }
+                    break;
+            }
+        }
+
+        private void verMinas() {
+            for (int i = 1; i < N-1; i++)
+                for (int j = 1; j < M-1; j++)
+                    if (m[i, j] == 9) campoBotones[i, j].Text = "B";
+        }
+
+        private bool esGanador() {
+            for (int i = 1; i < N-1; i++)
+                for (int j = 1; j < M-1; j++)
+                    if (m[i,j] == 9 && campoBotones[i,j].Text!="x") return false;
+            return true;
+        }
+
+        private void cuentaMinas() {
+            ////
+            ////
+            //// Código a Completar
+            ////
+            ////
+        }  
+
+        private void ubicaMinas()
+        {
+            ////
+            ////
+            //// Código a Completar
+            ////
+            ////
+        }
+
+        private void escampa(int x, int y) {
+            ////
+            ////
+            //// Código recursivo a Completar
+            ////
+            ////
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            ReiniciaControl();
+            N = (int) nfilasNUD.Value + 2; // 2 filas extras para facilitar las busquedas
+            M = (int) ncolumnasNUD.Value + 2;  // 2 columnas extras para facilitar las búsquedas
+            Porcentaje = (int) porcentajeMinasNUD.Value;
+            Form1.ActiveForm.Left = 0;
+            Form1.ActiveForm.Top = 0;
+            Form1.ActiveForm.Width = 25 * (M - 2) + 36;
+            Form1.ActiveForm.Height = 25 * (N - 2) + 130;
+            ReiniciaJuego();
+        }
+    }
+}
+
