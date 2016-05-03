12a6b57dd9c104d44d53e4a6d483578f8a491f3c
ImSolar <isolarhd@gmail.com>
2016-05-03 12:27:46 +0200

    Entregable 10 prueba

---
+++ Entregables/ENTREGABLE10/ENTREGABLE10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTREGABLE10
+{
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+        }
+        public void Ejemplo(int a, int[] x)
+        {
+            if (a > 0)
+                for (int i = 0; i < x.Length; i++)
+                    x[i]++;
+
+            else
+                for (int i = 0; i < x.Length; i++)
+                    x[i] = 0;
+
+
+        }
+    }
+}
